cda53dadc30ccad0f8438aa7087d34d27ed7a887
Lee Cheng Wei <cw-l@users.noreply.github.com>
2026-04-09 00:00:15 +0800

    Removed unnecessary imports.

--- cmd/gen-test-data/main.go
+++ cmd/gen-test-data/main.go
@@ -1,13 +1,5 @@
 package main
 
-import (
-	"log"
-	"github.com/cw-l/ip2proxy-lite-cidr-convert"
-)
-
 func main() {
-	err := converter.GenerateExceptionData("./testdata/samples")
-	if err != nil {
-		log.Fatal(err)
-	}
+	GenerateExceptionData("./testdata/samples")
 }
